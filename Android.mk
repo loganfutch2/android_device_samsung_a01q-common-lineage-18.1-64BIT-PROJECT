9c2740efaa9569bf62f0f7a8a9b5c4c3b1703e27
loganfutch2 <49177644+loganfutch2@users.noreply.github.com>
2022-04-07 22:53:44 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter pine olives olive,$(TARGET_DEVICE)),)
+ifneq ($(filter a01q,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
